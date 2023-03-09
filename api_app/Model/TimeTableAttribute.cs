51b6fe968f9ee97d78c9756578825ac2686dffbe
Krish <lodha9krish@gmail.com>
2023-03-09 16:47:28 +0530

    initial commit

---
+++ api_app/Model/TimeTableAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace api_app.Model
+{
+    public class TimeTableAttributes
+    {
+        public string? subject { get; set; }
+        public string? term { get; set; }
+        public string? division { get; set; }
+        public string? lh { get; set; }
+    }
+}
